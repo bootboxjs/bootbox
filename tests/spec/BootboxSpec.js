633b77c8766e4d1266452405f345196fbedc0a10
Nick Payne <nick@kurai.co.uk>
2011-11-09 23:19:50 +0000

    adding bootstrap files to test vendor dir and very simple alert test (refs #5)

--- tests/spec/BootboxSpec.js
+++ tests/spec/BootboxSpec.js
@@ -1,9 +1,30 @@
 describe("Bootbox", function() {
-    it("exists in the global namespace", function() {
-        expect(bootbox).toBeDefined();
+
+    describe("Initial state", function() {
+        it("exists in the global namespace", function() {
+            expect(bootbox).toBeDefined();
+        });
+
+        it("starts with no visible dialogs", function() {
+            expect($(".bootbox")).not.toExist();
+        });
     });
 
-    it("starts with no visible dialogs", function() {
-        expect($(".bootbox")).not.toExist();
+    describe("Alerts", function() {
+        var box;
+
+        beforeEach(function() {
+            box = null;
+        });
+
+        afterEach(function() {
+            // remove box cleanly
+        });
+
+        it("shows an OK button when invoked with a single argument", function() {
+            box = bootbox.alert("Hello world!");
+
+            expect($("a", box)).toHaveText("OK");
+        });
     });
 });
