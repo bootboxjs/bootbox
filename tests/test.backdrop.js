d5046adb5db966e638e3c330649ecfaab4227a01
Nick Payne <nick@kurai.co.uk>
2012-09-23 16:47:12 +0100

    add tests around #backdrop method

---
+++ tests/test.backdrop.js
@@ -0,0 +1,46 @@
+describe("backdrop", function() {
+    var box;
+
+    before(function() {
+        bootbox.backdrop(false);
+    });
+
+    after(function() {
+        $(".bootbox")
+        .modal('hide')
+        .remove();
+    });
+
+    describe("when disabled", function() {
+        before(function() {
+            box = bootbox.dialog("foo");
+        });
+
+        it("should not add the backdrop element", function() {
+            assert.equal(box.next(".modal-backdrop").length, 0);
+        });
+
+        describe("but when passed as an option", function() {
+            before(function() {
+                box = bootbox.dialog("foo", [], {"backdrop": true});
+            });
+
+            it("should add the backdrop element", function() {
+                assert.equal(box.next(".modal-backdrop").length, 1);
+            });
+        });
+
+    });
+
+    describe("when enabled", function() {
+        before(function() {
+            bootbox.backdrop(true);
+            box = bootbox.dialog("foo");
+        });
+
+        it("should add the backdrop element", function() {
+            assert.equal(box.next(".modal-backdrop").length, 1);
+        });
+    });
+
+});
