cc3ca7a12f5f7f07cd32fc3bb8bd0f8e563cd708
Michael Day <manveru.alma@gmail.com>
2013-07-23 17:58:05 -0500

    Adding type=text back to input

--- bootbox.js
+++ bootbox.js
@@ -212,7 +212,7 @@ var bootbox = window.bootbox || (function(document, $) {
 
         // let's keep a reference to the form object for later
         var form = $("<form></form>");
-        form.append("<input autocomplete=off class=text value='" + defaultVal + "' />");
+        form.append("<input autocomplete=off class=text type=text value='" + defaultVal + "' />");
 
         var cancelCallback = function() {
             if (typeof cb === 'function') {
