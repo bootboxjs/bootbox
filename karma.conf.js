2e1ede37c9029a1871a2a1ba59d39e05c12ed984
Nick Payne <nick@kurai.co.uk>
2013-08-21 20:20:50 +0100

    add karma-chai package

--- karma.conf.js
+++ karma.conf.js
@@ -2,13 +2,10 @@ module.exports = function(config) {
 
   return config.set({
     basePath: "",
-    frameworks: ["mocha"],
+    frameworks: ["mocha", "chai"],
     files: [
-      "node_modules/chai/chai.js",
       "tests/vendor/jquery-1.8.3.min.js",
       "tests/vendor/bootstrap-3.0.0-wip.js",
-      // awkward name; we mean the bootstrap process, not the library!
-      "tests/bootstrap.js",
       "bootbox.js",
       "tests/**/*.test.coffee",
       "tests/**/*.test.js"
--- tests/bootstrap.js
+++
@@ -1,1 +0,0 @@
-var expect = chai.expect;
