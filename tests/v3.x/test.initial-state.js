1a5e42c68d217bb4527341e5d4347a4503b9d7ba
Nick Payne <nick@kurai.co.uk>
2013-08-28 20:28:25 +0100

    remove v3.x tests

--- tests/v3.x/test.initial-state.js
+++
@@ -1,9 +0,0 @@
-describe("Initial state", function() {
-    it("exists in the global namespace", function() {
-        assert.ok(bootbox);
-    });
-
-    it("starts with no dialogs in the DOM", function() {
-        assert.equal($(".bootbox").length, 0);
-    });
-});
