9854f5a5bb683248a7330cddfb821af23307b903
eslam salem <netcodez@gmail.com>
2014-12-23 08:10:10 +0200

    add arabic language support in locale

--- bootbox.js
+++ bootbox.js
@@ -782,6 +782,11 @@
    * unlikely to be required. If this gets too large it can be split out into separate JS files.
    */
   var locales = {
+    ar : {
+      OK      : "حسنا",
+      CANCEL  : "إلغاء",
+      CONFIRM : "موافق"
+    },
     bg_BG : {
       OK      : "Ок",
       CANCEL  : "Отказ",
