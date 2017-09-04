ce741d01f9ab499ef419f824185d03a9c20d34e2
Tieson Trowbridge <tiesont@gmail.com>
2017-09-04 04:05:02 -0400

    Updated wrong config

--- karma.conf.bs4-beta.js
+++
@@ -1,9 +0,0 @@
-var baseConfig = require('./karma-base.conf');
-
-module.exports = baseConfig({
-  vendor: [
-    'tests/vendor/jquery-1.11.2.min.js',
-    'tests/vendor/popper.min.js',
-    'tests/vendor/bootstrap-4.0.0-beta.min.js'
-  ]
-});
--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,7 @@ var baseConfig = require('./karma-base.conf');
 module.exports = baseConfig({
   vendor: [
     'tests/vendor/jquery-1.11.2.min.js',
+    'tests/vendor/popper.min.js',
     'tests/vendor/bootstrap-4.0.0-beta.min.js'
   ],
   src: ['src/bootbox.js', 'src/locales.js']
