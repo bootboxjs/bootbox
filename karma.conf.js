aba0169baa966bab6c6862aafcf5d6dd4f24d41e
Tieson Trowbridge <ttrowbridge@arivium.com>
2018-04-12 15:51:57 -0400

    Reverting to BS 4.0

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ var baseConfig = require('./karma-base.conf');
 module.exports = baseConfig({
   vendor: [
     'tests/vendor/jquery-3.3.1.slim.min.js',
-    'tests/vendor/bootstrap-4.1.0.bundle.min.js'
+    'tests/vendor/bootstrap-4.0.0.bundle.min.js'
   ],
   src: ['src/bootbox.js', 'src/bootbox.locales.js']
 });
