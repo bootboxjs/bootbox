6e2a31f599ac3f7e752dbbd5e67fa1d0c77985e4
Tieson Trowbridge <ttrowbridge@arivium.com>
2018-04-12 15:38:36 -0400

    Switch to bundled version of Bootstrap

--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,7 @@ var baseConfig = require('./karma-base.conf');
 module.exports = baseConfig({
   vendor: [
     'tests/vendor/jquery-3.3.1.slim.min.js',
-    'tests/vendor/popper.min.js',
-    'tests/vendor/bootstrap-4.1.0.min.js'
+    'tests/vendor/bootstrap-4.1.0.bundle.min.js'
   ],
   src: ['src/bootbox.js', 'src/bootbox.locales.js']
 });
