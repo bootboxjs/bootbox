038b4b407bea8757b5a3b6dbec17b2154b1ac445
Tieson Trowbridge <ttrowbridge@arivium.com>
2019-02-27 16:29:44 -0500

    Updates docs for v5.x

---
+++ v4.x/js/example.js
@@ -0,0 +1,24 @@
+/**
+ * This tiny script just helps us demonstrate
+ * what the various example callbacks are doing
+ */
+var Example = (function() {
+    "use strict";
+
+    var elem,
+        hideHandler,
+        that = {};
+
+    that.init = function(options) {
+        elem = $(options.selector);
+    };
+
+    that.show = function(text) {
+        clearTimeout(hideHandler);
+
+        elem.find("span").html(text);
+        elem.delay(200).fadeIn().delay(4000).fadeOut();
+    };
+
+    return that;
+}());
