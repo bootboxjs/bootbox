d01874c109b62fa43d868de97d9ae928deeeb1ee
bluedasher13 <40885356+bluedasher13@users.noreply.github.com>
2019-10-27 18:01:49 +0800

    fix Win7+IE11 dialog 100% height problem
    add .modal-dialog { align-items: center; }

--- bootbox.all.js
+++ bootbox.all.js
@@ -274,7 +274,7 @@
   var templates = {
     dialog:
     '<div class="bootbox modal" tabindex="-1" role="dialog" aria-hidden="true">' +
-    '<div class="modal-dialog">' +
+    '<div class="modal-dialog" style="align-items: center;">' +
     '<div class="modal-content">' +
     '<div class="modal-body"><div class="bootbox-body"></div></div>' +
     '</div>' +
