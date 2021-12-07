eca4d4b6f98582a5e6a17634568b93f3ef8a8a1f
Taikono-Himazin <kazu@po.harenet.ne.jp>
2021-12-07 10:47:59 +0900

    hideAll enhancements
    Changed to automatically close the modal that hideAll is transitioning after the transition is completed.
    An optional argument has been added to hideAll to make it behave the same as before.

--- bootbox.js
+++ bootbox.js
@@ -225,7 +225,8 @@
 
 
   // Hides all currently active Bootbox modals
-  exports.hideAll = function () {
+  exports.hideAll = function (closeTransitioning=false) {
+    if(closeTransitioning){$('.bootbox').on('shown.bs.modal', (event) => { $(event.target).modal('hide'); })}
     $('.bootbox').modal('hide');
 
     return exports;
